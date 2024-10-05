debc224ab6bf766de293813604d2551315bc98a9
Adrian Garza <adrianhgarza@gmail.com>
2024-10-04 22:57:03 -0700

    less code

--- plexsync.py
+++ plexsync.py
@@ -18,25 +18,20 @@ def etree_to_dict(t):
 def to_dict(r):
     return etree_to_dict(ElementTree.fromstring(r.content))
 
-def load_mbee_playlists() -> list[str]:
-    return glob.glob("E:\MusicBee\Playlists\*.m3u")
-
 # get this from the plex_tv_auth cookie under authentication_token
 # is this ok to use? i mean it works but still...not documented anywhere lol
-token = os.environ["TOKEN"]
 s = requests.Session()
 s.headers = {
-	"X-Plex-Token": token,
+	"X-Plex-Token": os.environ["TOKEN"],
 }
 
-disk_playlists = load_mbee_playlists()
 # https://plexapi.dev/api-reference/playlists/upload-playlist
-for p in disk_playlists:
+for p in glob.glob("E:\MusicBee\Playlists\*.m3u"):
     print("updating playlist "+p)
     response = s.post(
         base_url+"/playlists/upload",
         params={"path":p, "sectionID": 1},
 	)
-    print(response)
+    print(response.status_code)
     if not response:
         print(response.content)
